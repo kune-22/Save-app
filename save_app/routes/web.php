cf848905d1ffe02a470356b526dad404ed50c0dc
野良猫 <ruirudo@icloud.com>
2025-09-21 02:03:11 +0900

    create all DB

--- save_app/routes/web.php
+++ save_app/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SavesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [SavesController::class, 'index'])->name('home');
